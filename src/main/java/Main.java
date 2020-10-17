1e0beb4b5a9b068e524f9cd3df7b42b862cc20a8
bertox94 <37112695+bertox94@users.noreply.github.com>
2020-10-17 18:24:12 +0200

    OK

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -58,7 +58,7 @@ public class Main {
                     "    }\n" +
                     "  };\n" +
                     "  xhttp.open(\"GET\", \"47450596\", true);\n" +
-                    "  xhttp.send();} setInterval(func, 4900);" +
+                    "  xhttp.send();\n} \nsetInterval(func, 900);" +
                     "</script>" +
                     "</html>";
             httpExchange.sendResponseHeaders(200, response.length());
